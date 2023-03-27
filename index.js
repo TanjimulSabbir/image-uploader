6b4c35e79e05edfc1c5de0ac05c01e17210e26a4
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-03-27 22:14:12 +0600

    second commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 document.getElementById("form").addEventListener("submit", handleSubmit = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    const Title = event.target.title.value;
+    const password = event.target.description.value;
     const file = event.target.image.files[0];
     const reader = new FileReader();
 
     // Set up the reader to read the file as a data URL
     reader.readAsDataURL(file);
-
     // When the reader has loaded the file, create an image element with the data URL
     reader.onload = function () {
         const image = document.createElement('img');
-        image.classList.add("rounded", "h-full", "md:h-[300px]", "w-full")
+        image.classList.add("rounded", "h-full", "md:h-[300px]", "w-full", "hover:opacity-70", "transition", "duration-200");
         image.src = reader.result;
 
         // Append the image to the image preview div
         document.getElementById("imageDisplay").appendChild(image);
     };
+
 }
 )
